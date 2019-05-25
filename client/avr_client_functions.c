c2a7b548f26a6bdd627f335f3bf9bc866a83c342
ninniks <nicolaninni97@gmail.com>
2019-05-25 16:20:22 +0200

    last release

--- client/avr_client_functions.c
+++ client/avr_client_functions.c
@@ -10,12 +10,11 @@
 #include "loop.h"
 #include <stdbool.h>
 
-#define SNAME "sname:"
-#define SCNAME "scname:"
-#define SCVAL "scval:"
-#define QYCH "qych:"
+#define DIGITAL_OUT "0"
+#define DIGITAL_IN  "1"
+#define ANALOG "2"
+#define ACK "ack"
 
-int fd;
 const char *ack = "ok";
 const char *handshake = "ACMino";
 const char *portname = "/dev/ttyACM0";
@@ -23,18 +22,44 @@ char buf = '\0';
 char string[512];
 char name[256];
 bool name_is_set = false;
+int fd;
 //builtin functions of smart house
 
 int set_name(char **args);
 int set_channel_name(char **args);
 int set_channel_value(char **args);
 int query_channels(char **args);
-int help(char **args);
+int get_channel_value(char **args);
+int get_adc_channel_value(char **args);
 
 
-char *user_switches_name[8] = { NULL };
+char *user_dout_channels_names[8] = { NULL };
+char *user_din_channels_names[8] = { NULL };
+char *user_adc_channels_names[8] = { NULL };
 
-char *default_switches_name[] = {
+char *default_adc_channels_name[] = {
+  "analog_in_0",
+  "analog_in_1",
+  "analog_in_2",
+  "analog_in_3",
+  "analog_in_4",
+  "analog_in_5",
+  "analog_in_6",
+  "analog_in_7",
+};
+
+char *default_din_channels_name[] = {
+  "digital_in_0",
+  "digital_in_1",
+  "digital_in_2",
+  "digital_in_3",
+  "digital_in_4",
+  "digital_in_5",
+  "digital_in_6",
+  "digital_in_7"
+};
+
+char *default_dout_channels_name[] = {
   "switch_0",
   "switch_1",
   "switch_2",
@@ -50,7 +75,10 @@ char *builtin_str[] = {
   "set_channel_name",
   "set_channel_value",
   "query_channels",
-  "help"
+  "get_channel_value",
+  "get_adc_channel_value",
+  "help",
+  "exit"
 };
 
 int (*builtin_func[]) (char **) = {
@@ -58,7 +86,8 @@ int (*builtin_func[]) (char **) = {
   &set_channel_name,
   &set_channel_value,
   &query_channels,
-  &help
+  &get_channel_value,
+  &get_adc_channel_value
 };
 
 int avr_connection_init(){
@@ -72,13 +101,6 @@ int avr_connection_init(){
 
   serial_init(fd, B19200, 0);
   set_blocking(fd, 0);
-
-  /*write(fd, handshake, sizeof(handshake));
-
-  read(fd, &buf, sizeof(ack));
-
-  if(buf == *ack)
-    return 0;*/
   return 1;
 }
 
@@ -86,29 +108,25 @@ int avr_client_num_builtins() {
   return sizeof(builtin_str) / sizeof(char *);
 }
 
-int avr_default_switches_num(){
-  return sizeof(default_switches_name)/ sizeof(char *);
+int avr_channels_num(char **channels){
+  return sizeof(channels)/ sizeof(char );
 }
 
-int avr_user_switches_num(){
-  return sizeof(user_switches_name)/sizeof(char *);
-}
-
-int avr_get_conf_switch(char **args){
+int avr_get_conf_switch(char **args, char **channels){
   int i;
-  for(i = 0; i < avr_user_switches_num(); i++){
-    if(user_switches_name[i] != NULL){
-      if(strcmp(args[1], user_switches_name[i]) == 0)
+  for(i = 0; i < avr_channels_num(channels); i++){
+    if(channels[i] != NULL){
+      if(strcmp(args[1], channels[i]) == 0)
         return i;
     }
   }
   return -1;
 }
 
-int avr_check_switches_name(char **args){
+int avr_check_switches_name(char **args, char **channels){
   int i;
-  for(i = 0; i < avr_default_switches_num(); i++){
-    if(strcmp(args[1], default_switches_name[i]) == 0){
+  for(i = 0; i < avr_channels_num(channels); i++){
+    if(strcmp(args[1], channels[i]) == 0){
       return i;
     }
   }
@@ -116,13 +134,28 @@ int avr_check_switches_name(char **args){
   return -1;
 }
 
-void print_user_channels(){
+int avr_chech_user_channel_input(char **args, char *channel_name, int size){
+  if(args[2] != NULL && (strncmp(channel_name, args[1], size) == 0)){
+    return 0;
+  }
+  return 1;
+}
+
+void avr_write_channel_name(char **args, char *channels[], int p){
+  if(channels[p] == NULL){
+    channels[p] = (char *) malloc(sizeof(args[2])*sizeof(char));
+    strcpy(channels[p], args[2]);
+    printf("Done.\n");
+  }
+}
+
+void print_channels(char *channels[]){
   int c;
-  for(c = 0; c < avr_user_switches_num(); c++){
-    if(user_switches_name[c] == NULL){
+  for(c = 0; c < avr_channels_num(channels); c++){
+    if(channels[c] == NULL){
       printf("empty\n");
     }else{
-      printf("%s\n", user_switches_name[c]);
+      printf("%s\n", channels[c]);
     }
   }
 }
@@ -131,8 +164,6 @@ int set_name(char **args){
   if(args[1] == NULL){
     fprintf(stderr, "avr_client: expected argument to \"set_name\"\n");
   }else{
-    //snprintf(string, sizeof string, "%s%s%s%s", SNAME, args[1],";","\r");
-    //write(fd,string, strlen(string));
     snprintf(name, sizeof name, "%s", args[1]);
     name_is_set = true;
   }
@@ -144,21 +175,32 @@ int set_channel_name(char **args){
   if(args[1] == NULL){
     fprintf(stderr, "avr_client: expected argument to \"set_channel_name\"\n");
   }else if(name_is_set){
-    if(args[2] != NULL){
-      p = avr_check_switches_name(args);
+    if(avr_chech_user_channel_input(args, "switch_", 7) == 0){
+      p = avr_get_conf_switch(args, default_dout_channels_name);
       if(p != -1){
-        if(user_switches_name[p] == NULL){
-          user_switches_name[p] = (char *) malloc(sizeof(args[2])*sizeof(char));
-          strcpy(user_switches_name[p], args[2]);
-        }
+        avr_write_channel_name(args, user_dout_channels_names, p);
       }else{
         fprintf(stderr,"avr_client: no channnel named: %s\n",args[1]);
       }
-    }else {
-      fprintf(stderr, "avr_client: \"set_channel_name\" usage: <switch_(n)> <name>\n");
+    }else if(avr_chech_user_channel_input(args, "digital_in_", 11) == 0) {
+      p = avr_get_conf_switch(args, default_din_channels_name);
+      if(p != -1){
+        avr_write_channel_name(args, user_din_channels_names, p);
+      }else{
+        fprintf(stderr,"avr_client: no channnel named: %s\n",args[1]);
+      }
+    }else if(avr_chech_user_channel_input(args, "analog_in_", 10) == 0){
+      p = avr_get_conf_switch(args, default_adc_channels_name);
+      if(p != -1){
+        avr_write_channel_name(args, user_adc_channels_names, p);
+      }else{
+        fprintf(stderr,"avr_client: no channnel named: %s\n",args[1]);
+      }
+    }else{
+      fprintf(stderr, "avr_client: \"set_channel_name\" usage: <channelDefaultName_(n)> <name>. \n");
     }
   }else{
-    fprintf(stderr, "avr_client: please set a name to the device. Use \"help\" for info\n");
+    fprintf(stderr, "avr_client: please set a name to the device. \n");
   }
 }
 
@@ -167,16 +209,17 @@ int set_channel_value(char **args){
   if(args[1] == NULL){
     fprintf(stderr, "avr_client: expected argument to \"set_channel_value\"\n");
   }else if(name_is_set){
-    pos = avr_get_conf_switch(args);
-    printf("%d", pos);
+    pos = avr_get_conf_switch(args, user_dout_channels_names);
     if(pos != -1){
       if((strcmp(args[2],"0") != 0) && (strcmp(args[2], "1")!= 0)){
         fprintf(stderr, "avr_client: invalid switch value.\n");
       }else{
+        char *s_checksum = malloc(4*sizeof(char));
         char c[256];
         sprintf(c,"%d",pos);
-        snprintf(string, sizeof string, "%s%s%s%s%s", c , ":", args[2], ";","\r");
+        snprintf(string, sizeof string, "%s%s%s%s%s%s%s", DIGITAL_OUT, ":", c , ":", args[2], ";", "\r");
         write(fd, string, strlen(string));
+        read(fd, string, 4)
      }
     }else{
       fprintf(stderr, "avr_client: no switch with name: %s", args[1]);
@@ -186,18 +229,52 @@ int set_channel_value(char **args){
   }
 }
 
-int query_channels(char **args){
+int get_channel_value(char **args){
+  int pos;
   if(args[1] == NULL){
-    fprintf(stderr, "avr_client: expected argument to \"query_channels\"\n");
-  }
+    fprintf(stderr, "avr_client: expected argument to \"get_channel_value\"\n");
+  }else if(name_is_set){
+    pos = avr_get_conf_switch(args, user_din_channels_names);
+    if(pos != -1){
+        char c[256];
+        sprintf(c,"%d",pos);
+        snprintf(string, sizeof string, "%s%s%s%s%s%s%s", DIGITAL_IN, ":", c , ":", args[2], ";","\r");
+        write(fd, string, strlen(string));
+        read(fd, string, sizeof(1));
+     }
+    }else{
+      fprintf(stderr, "avr_client: no switch with name: %s", args[1]);
+    }
 }
 
-int help(char **args){
+int get_adc_channel_value(char **args){
+  int pos;
   if(args[1] == NULL){
-    fprintf(stderr, "avr_client: expected argument to \"set_name\"\n");
-  }else{
-      print_user_channels();
-  }
+    fprintf(stderr, "avr_client: expected argument to \"get_channel_value\"\n");
+  }else if(name_is_set){
+    pos = avr_get_conf_switch(args, user_adc_channels_names);
+    if(pos != -1){
+        char c[256];
+        char res[3];
+        sprintf(c,"%d",pos);
+        snprintf(string, sizeof string, "%s%s%s%s%s%s%s", ANALOG, ":", c , ":", "0", ";","\r");
+        write(fd, string, strlen(string));
+        read(fd, res, 4);
+        printf("%s", res);
+        memset(&res, 0, sizeof(res));
+     }
+    }else{
+      fprintf(stderr, "avr_client: no switch with name: %s", args[1]);
+    }
+}
+
+int query_channels(char **args){
+  printf("\n DIGITAL OUT:\n");
+  print_channels(user_dout_channels_names);
+  printf("\n DIGITAL IN:\n");
+  print_channels(user_din_channels_names);
+  printf("\n ANALOG IN:\n");
+  print_channels(user_adc_channels_names);
 }
 
 
@@ -213,6 +290,8 @@ int avr_client_execute(char **args){
      return (*builtin_func[i])(args);
    }
  }
+
+ fprintf(stderr, "avr_client: command not found. Please use \"help\" for info.\n");
 
  return 1;
 }
--- server/main.c
+++ server/main.c
@@ -8,44 +8,107 @@
 #include <stdint.h>
 #include "serial.h"
 #define SIZE 256
-
-//volatile uint8_t * const data = (uint8_t *) 0x25;
-//volatile uint8_t * const dir  = (uint8_t *) 0x24;
+#define DIGITAL_OUT 0
+#define DIGITAL_IN 1
+#define ANALOG 2
+#define INPUT_TYPE 0
+#define CHANNEL 1
+#define VALUE 2
+#define ACK "ack"
+#define NACK "nack"
 
 //macros for handling memory addresses of I/O ports
-#define PORTA   (* (volatile uint8_t *) 0x22)
-#define PORTB   (* (volatile uint8_t *) 0x25)
-#define DDRA    (* (volatile uint8_t *) 0x21)
-#define DDRB    (* (volatile uint8_t *) 0x24)
 
-uint8_t mask;
+#define PORTA   (* (volatile uint8_t *) 0x22) //PORTA is used for digital in
+#define PORTB   (* (volatile uint8_t *) 0x25) //PORTB is used for digital out
+#define DDRA    (* (volatile uint8_t *) 0x21) //Data Direction Register to setup I/O
+#define DDRB    (* (volatile uint8_t *) 0x24)
+#define PINA    (* (volatile uint8_t *) 0x20) //PORTA input pins
+#define ADMUX   (* (volatile uint8_t *) 0x7C) //ADC Multiplexer section register
+#define ADCSRA  (* (volatile uint8_t *) 0x7A) //ADC Status and Control Register
+#define ADC     (* (volatile uint16_t*) 0x78) //ADC Data Register 16 bit
+
+
+#define REFS0  6 //ADMUX bit 0 select voltage reference for ADC
+#define ADPS0  0 //ADCSRA bits. These bits determine the division factor between the XTAL frequency and the input clock to the ADC.
+#define ADPS1  1
+#define ADPS2  2
+#define ADEN   7 //ADCSRA bit 7 set to 1 enables the ADC
+#define ADSC   6 //ADC start conversion
+
+uint8_t o_mask;
+uint8_t i_mask;
 
 void set_output_channels(void){
   const uint8_t out = 255;
   DDRB |= out;
 }
 
-void parse_string(uint8_t* src, uint8_t* key, uint8_t* value){
-  while(*src != ':'){
-    uint8_t c = *src;
-    *key = c;
-    ++src;
-    ++key;
-  }
-  *key='\0';
-
-  ++src;
-
-  while(*src && *src != ';'){
-    uint8_t c = *src;
-    *value = c;
-    ++src;
-    ++value;
-  }
-  *value='\0';
-  return;
+void set_input_channels(void){
+  const uint8_t in = 0x00;
+  DDRA = in;
 }
 
+
+void ADC_init(void){
+  // Select Vref=AVcc
+  ADMUX |= (1<<REFS0);
+  //set prescaller to 128 and enable ADC
+  ADCSRA |= (1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0)|(1<<ADEN);
+
+}
+
+uint16_t adc_read(uint8_t channel){
+  //select ADC channel
+  ADMUX = (ADMUX & 0xF0) | (channel & 0x0F);
+  //single conversion mode
+  ADCSRA |= (1<<ADSC);
+  // wait until ADC conversion is complete
+  while(ADCSRA & (1<<ADSC));
+
+  return ADC;
+}
+
+
+void parse_string(uint8_t* src, uint8_t* type, uint8_t* channel, uint8_t* value, uint8_t* chksum){
+  int word = 0;
+
+  while(*src != ';'){
+    uint8_t c = *src;
+    if(*src == ':'){
+      ++src;
+      c = *src;
+      word+=1;
+    }
+
+    switch(word){
+      case INPUT_TYPE:
+        *type = c;
+        ++src;
+        ++type;
+        break;
+      case CHANNEL:
+        *channel = c;
+        ++src;
+        ++channel;
+        break;
+      case VALUE:
+        *value = c;
+        ++src;
+        ++value;
+        break;
+      case CHECKSUM:
+        *chksum = c;
+        ++src;
+        ++chksum;
+        break;
+    }
+  }
+  *type = '\0';
+  *channel = '\0';
+  *value = '\0';
+  *chksum = '\0';
+}
 
 int8_t my_strcmp(const uint8_t* s1, const uint8_t* s2){
   while((*s1 != '\0') && (*s1 == *s2)){
@@ -57,25 +120,51 @@ int8_t my_strcmp(const uint8_t* s1, const uint8_t* s2){
 
 int main(void){
   uint8_t buf[SIZE];
-  uint8_t key[SIZE];
+  uint8_t channel[SIZE];
   uint8_t value[SIZE];
+  uint8_t type[SIZE];
 
+  char mychar[5];
   serial_init();
   set_output_channels();
+  set_input_channels();
+  ADC_init();
+
 
   while(1){
     serial_get_string(buf);
-    parse_string(buf, key, value);
-    serial_put_string(key);
-    serial_put_string(value);
-    int bit = key[0]-'0';
-    serial_put_string((uint8_t *) bit);
-    mask = (1<<bit);
-    if(my_strcmp((uint8_t *)"1",value) == 0){
-      PORTB |= (1<<bit);
-      serial_put_string((uint8_t *) "on\n");
-    }else if(my_strcmp((uint8_t *)"0", value) == 0){
-      PORTB &= ~(1 << (bit));
+    parse_string(buf, type, channel, value, checksum);
+
+    uint16_t adc_value;
+    int t = type[0]-'0';
+    int bit = channel[0]-'0';
+
+    switch(t){
+      case DIGITAL_OUT:
+        o_mask = (1<<bit);
+        if(my_strcmp((uint8_t *)"1",value) == 0){
+          PORTB |= (1<<bit);
+          serial_put_string((uint8_t *) ACK);
+        }else if(my_strcmp((uint8_t *)"0", value) == 0){
+          PORTB &= ~(1 << (bit));
+          serial_put_string((uint8_t *) ACK);
+        }
+      break;
+
+      case DIGITAL_IN:
+        i_mask = (1<<bit);
+        if((PINA & (1<< bit)) == i_mask){
+          serial_put_string((uint8_t *) "1");
+        }else{
+          serial_put_string((uint8_t *) "0");
+        }
+      break;
+
+      case ANALOG:
+        adc_value = adc_read(bit);
+        sprintf(mychar,"%04d",adc_value);
+        serial_put_string((uint8_t *) *(&mychar));
+      break;
     }
   }
 }
