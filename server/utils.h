aabc6d7fb4b63392c318e2b36723c247fe321e20
ninniks <nicolaninni97@gmail.com>
2019-06-07 14:47:36 +0200

    code update

---
+++ server/utils.h
@@ -0,0 +1,30 @@
+#define SIZE 256
+#define DIGITAL_OUT 0
+#define DIGITAL_IN 1
+#define ANALOG 2
+#define NAME 0
+#define INPUT_TYPE 1
+#define CHANNEL 2
+#define VALUE 3
+#define ACK "ack"
+#define NACK "nack"
+#define SNAME "sname"
+
+//macros for handling memory addresses of I/O ports
+
+#define PORTA   (* (volatile uint8_t *) 0x22) //PORTA is used for digital in
+#define PORTB   (* (volatile uint8_t *) 0x25) //PORTB is used for digital out
+#define DDRA    (* (volatile uint8_t *) 0x21) //Data Direction Register to setup I/O
+#define DDRB    (* (volatile uint8_t *) 0x24)
+#define PINA    (* (volatile uint8_t *) 0x20) //PORTA input pins
+#define ADMUX   (* (volatile uint8_t *) 0x7C) //ADC Multiplexer section register
+#define ADCSRA  (* (volatile uint8_t *) 0x7A) //ADC Status and Control Register
+#define ADC     (* (volatile uint16_t*) 0x78) //ADC Data Register 16 bit
+
+
+#define REFS0  6 //ADMUX bit 0 select voltage reference for ADC
+#define ADPS0  0 //ADCSRA bits. These bits determine the division factor between the XTAL frequency and the input clock to the ADC.
+#define ADPS1  1
+#define ADPS2  2
+#define ADEN   7 //ADCSRA bit 7 set to 1 enables the ADC
+#define ADSC   6 //ADC start conversion
